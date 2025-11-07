08be7c7b2b947382f97ae445293f92d03937bc08
rajwanir2 <rahim.rajwani@nih.gov>
2025-11-07 17:34:39 -0500

    Adds fastq.gz size estimation

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from shiny import reactive
 from shiny.express import input, render, ui
 import shinyswatch
 from shinywidgets import render_plotly
-from src.Functions import estimate_bam_size_from_nreads,file_size_converter, plot_cummulative_cost_over_years, reads_to_bases
+from src.Functions import estimate_bam_size_from_nreads,file_size_converter, plot_cummulative_cost_over_years, reads_to_bases, estimate_fastqz_size_from_nreads
 from faicons import icon_svg
 
 
@@ -79,7 +79,14 @@ with ui.sidebar(title=ui.span(icon_svg("gears"), "Parameters")):
             min=0.0,
             max=1.0,
             value=0.30)
-    
+
+        ui.input_numeric(
+            id="fastq_gzip_compression_ratio",
+            label="FASTQ gzip Compression Ratio",
+            min=0.0,
+            max=1.0,
+            value=0.25)
+
     ui.input_select(
         id="output_format",
         label="Output Format",
@@ -93,52 +100,66 @@ with ui.sidebar(title=ui.span(icon_svg("gears"), "Parameters")):
         step=0.0001
     )
 
-with ui.layout_column_wrap(fill=False):
-    with ui.value_box(theme="warning", showcase=icon_svg("file-zipper", style="solid")):
-        @render.text
-        def estimated_disk_usage_title():
-            return f"Estimated {input.output_format()} File Size"
-        @render.text
-        def estimated_bam_size():
-            bam_size_string = file_size_converter(estimated_bam_size_bytes())
-            return f"{bam_size_string}"
+with ui.layout_column_wrap(fill=True):
+
+    with ui.card(full_screen=True):
+        with ui.card(full_screen=True):
+            ui.h2(icon_svg("scale-unbalanced", style="solid")," Estimated File Sizes")
+            with ui.value_box(theme="success", showcase=icon_svg("file-zipper", style="solid")):
+                @render.text
+                def estimated_disk_usage_title():
+                    return f"Estimated {input.output_format()} File Size"
+                @render.text
+                def estimated_bam_size():
+                    bam_size_string = file_size_converter(estimated_bam_size_bytes())
+                    return f"{bam_size_string}"
+                
+            with ui.value_box(theme="success", showcase=icon_svg("file", style="solid")):
+                "Estimated fastq.gz File Size"
+                @render.text
+                def estimated_fastqz_size():
+                    fastqz_size_string = file_size_converter(estimated_fastqz_size_bytes())
+                    return f"{fastqz_size_string}"
+
+        #with ui.layout_column_wrap(fill=False):
+        with ui.card(full_screen=True):
+            ui.h2(icon_svg("info", style="solid"), "Inputs")
+            with ui.value_box(showcase_layout="top right", theme="info"):
+                "Number of Reads"
+                @render.text
+                def num_reads_display():
+                    return f"{input.num_reads()}"
+
+
+            with ui.value_box(showcase_layout="top right",  theme="info"):
+                "Number of Bases"
+                @render.text
+                def num_bases_display():
+                    num_bases = reads_to_bases(input.num_reads(), input.read_length())
+                    return f"{num_bases} bases"
+
+            with ui.value_box(showcase_layout="top right",  theme="info"):
+                "Read Length"
+                @render.text
+                def read_length_display():
+                    return f"{input.read_length()} nt"
+        
+    with ui.card(full_screen=True):
+        ui.h2(icon_svg("money-check-dollar", style="solid"), "Cost Estimates")
+        with ui.value_box(theme="warning", showcase=icon_svg("sack-dollar", style="solid")):
+            "Estimated Monthly Storage Cost"
+            @render.text
+            def estimated_monthly_cost_display():
+                return f"${estimated_monthly_cost():.2f} per month"
+            
+        with ui.card(full_screen=True):
+            @render_plotly
+            def cummulative_cost_chart():
+                return plot_cummulative_cost_over_years(
+                    monthly_cost=estimated_monthly_cost(),
+                    years=5
+                )
     
-    with ui.value_box(theme="warning", showcase=icon_svg("sack-dollar", style="solid")):
-        "Estimated Monthly Storage Cost"
-        @render.text
-        def estimated_monthly_cost_display():
-            return f"${estimated_monthly_cost():.2f} per month"
-
-with ui.card(full_screen=True):
-    @render_plotly
-    def cummulative_cost_chart():
-        return plot_cummulative_cost_over_years(
-            monthly_cost=estimated_monthly_cost(),
-            years=5
-        )
-
-with ui.layout_column_wrap(fill=False):
-    with ui.value_box(showcase_layout="top right", theme="light"):
-        "Number of Reads"
-        @render.text
-        def num_reads_display():
-            return f"{input.num_reads()}"
-
-
-    with ui.value_box(showcase_layout="top right",  theme="light"):
-        "Number of Bases"
-        @render.text
-        def num_bases_display():
-            num_bases = reads_to_bases(input.num_reads(), input.read_length())
-            return f"{num_bases} bases"
-
-    with ui.value_box(showcase_layout="top right",  theme="light"):
-        "Read Length"
-        @render.text
-        def read_length_display():
-            return f"{input.read_length()} nt"
-
-
 ## calculations
 @reactive.Calc
 def estimated_bam_size_bytes() -> float:
@@ -150,6 +171,14 @@ def estimated_bam_size_bytes() -> float:
                                                 output_format = input.output_format(),
                                                 cram_compression_ratio = input.cram_compression_ratio()
                                                 )
+
+@reactive.Calc
+def estimated_fastqz_size_bytes() -> float:
+    return estimate_fastqz_size_from_nreads(n_reads = input.num_reads(), 
+                                                read_len = input.read_length(),
+                                                gzip_compression_ratio = input.fastq_gzip_compression_ratio()
+                                                )
+
 @reactive.Calc
 def estimated_monthly_cost() -> float:
     size_in_gb = estimated_bam_size_bytes() / (1024 ** 3)
